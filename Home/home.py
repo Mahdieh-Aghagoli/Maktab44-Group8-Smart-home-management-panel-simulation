bbb7bda95de2da485e24c1103b64f455d0383f16
fatemeh.as <fatemehaminsafaei@gmail.com>
2020-11-20 23:29:57 +0330

    added logging

--- Home/home.py
+++ Home/home.py
@@ -1,14 +1,22 @@
 class Home:
-    def __init__(self,rooms, sensors, password):
+    def __init__(self, rooms, sensors, password):
+        def __init__(self, room_number):
+            logging.basicConfig(filename='IOT.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
+                                level=logging.INFO)
+            logging.info('Rooms and sensors!')
         self.rooms = rooms
         self.sensors = sensors  # i think there is no need for this!
         self.password = password
 
+    @property
     def __str__(self):
-        return"""
+        return """
         your rooms : {}
         sensors in your home : {}
         """.format(self.rooms, self.sensors)
+        logging.basicConfig(filename='IOT.log', filemode='w', format='%(levelname)s - %(asctime)s - %(message)s',
+                            level=logging.INFO)
+        logging.info(' !')
 
     @staticmethod
     def home_definition():
